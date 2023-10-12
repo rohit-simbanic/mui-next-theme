823dcbf3507fd6320dab277ad6c119ba4b75b699
rohit-simbanic <>
2023-10-12 18:05:45 +0530

    updated

--- app/component/header/index.tsx
+++ app/component/header/index.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import { styled } from "@mui/material/styles";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, useTheme } from "@mui/material";
 import LeftHeader from "./headerChildContainer/leftheader";
 import RightHeader from "./headerChildContainer/rightheader";
 import Logo from "./Logo";
 import DarkButton from "./darktoggle";
+import { tokens } from "../theme/theme";
 
 const StyledHeader = styled(AppBar)(({ theme }) => ({
-  backgroundColor: theme.color.headerBgColor,
   color: theme.palette.text.primary,
   display: theme.display.flex,
 }));
@@ -17,8 +17,13 @@ const Tool = styled(Toolbar)(({ theme }) => ({
 }));
 
 const Header = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <StyledHeader position="static">
+    <StyledHeader
+      position="static"
+      sx={{ backgroundColor: colors.primary[500] }}
+    >
       <Tool>
         <LeftHeader>
           <Logo />
